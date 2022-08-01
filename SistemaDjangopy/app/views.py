de0249a690c4cb883e518143e6bf1164a152ea4d
DouglasLourenze <douglas-zas@hotmail.com>
2022-08-01 11:23:01 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDjangopy/app/views.py
@@ -0,0 +1,13 @@
+from multiprocessing import context
+from urllib.request import Request
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from app import templates
+# Create your views here.
+
+def index(Request):
+    template = loader.get_template('app/home.html')
+    return HttpResponse(template.render())
+    #return HttpResponse("Hello world!")
+    #return render(Request, 'templates/home.html', context)
